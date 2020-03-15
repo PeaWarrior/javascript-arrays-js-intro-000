8036a364cb12df4572d9395a1b3da0e3ce0a27cc
Jackson Chen <jacksonchen171@gmail.com>
2020-03-15 22:51:23 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -25,7 +25,7 @@ function accessElementInArray(array, index){
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array){
-  array.unshift()
+  array = array.unshift()
   return array
 }
 
